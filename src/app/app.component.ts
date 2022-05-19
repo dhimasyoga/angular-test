e88beb8adaf6d8222f46d78ea623e468e0bb6d53
dhimasyoga <dhimasyoga14@gmail.com>
2022-05-19 14:02:23 +0700

    Test Progress (40%)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'user_type', 'entry', 'status'];
+  // dataSource: any = new MatTableDataSource([]);
+  dataSource:any = new MatTableDataSource([]);
+
+  @ViewChild(MatPaginator, {read: true}) paginator:any = MatPaginator;
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){    
+    this.httpClient.get("assets/mentor.json").subscribe(data =>{
+      this.dataSource = data;
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  // ngAfterViewInit() {
+  //   // this.dataSource.data = this.dataSource;
+  //   this.dataSource.paginator = this.paginator;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule, _MatTableDataSource} from '@angular/material/table';
+import {HttpClientModule} from "@angular/common/http";
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
